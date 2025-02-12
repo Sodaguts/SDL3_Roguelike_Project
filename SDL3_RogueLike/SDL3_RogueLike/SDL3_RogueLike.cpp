fd594ab1d80bf30f294561cb64efae96fa9e1154
celica lysik <celica.lysik@gmail.com>
2025-02-12 12:33:50 -0500

    Initial Commit

---
+++ SDL3_RogueLike/SDL3_RogueLike/SDL3_RogueLike.cpp
@@ -0,0 +1,12 @@
+﻿// SDL3_RogueLike.cpp : Defines the entry point for the application.
+//
+
+#include "SDL3_RogueLike.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
