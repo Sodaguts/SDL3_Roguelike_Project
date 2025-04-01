b7e96d1a717f5d7e7e6bb40124f5d0cc0d94befd
celica lysik <celica.lysik@gmail.com>
2025-03-31 21:40:38 -0400

    renamed files, cont. + GameObject.h, GameObject.cpp

---
+++ SDL3PROJECT/headers/GameObject.h
@@ -0,0 +1,40 @@
+#include <cstdio>
+#include <SDL3/SDL.h>
+
+//#include "../headers/Collider.h"
+
+class GameObject 
+{
+public:
+	GameObject()
+	{
+		m_playerCanMove = true;
+		m_playerStep = 120;
+		m_posX = 0;
+		m_posY = 0;
+		m_rect = { m_posX,m_posY,120,120 };
+		mp_sprite = nullptr;
+	};
+	~GameObject();
+	void update();
+	//void draw();
+	void handlePlayerInput(SDL_Event _event);
+
+	int getX() { return m_posX; };
+	int getY() { return m_posY; };
+	SDL_Rect& getRect() { return m_rect; };
+
+	void setSprite(SDL_Surface* p_sprite) { mp_sprite = p_sprite; };
+
+private:
+	//void handlePlayerInput(SDL_Event _event);
+	SDL_Rect m_rect;
+	int m_playerStep;
+	int m_posX;
+	int m_posY;
+	bool m_playerCanMove;
+
+	SDL_Surface* mp_sprite;
+
+
+};
---
+++ SDL3PROJECT/src/GameObject.cpp
@@ -0,0 +1,71 @@
+#include "../headers/GameObject.h"
+
+
+GameObject::~GameObject()
+{
+	SDL_DestroySurface(mp_sprite);
+}
+
+void GameObject::update()
+{
+	//handlePlayerInput(_event);
+
+}
+
+
+void GameObject::handlePlayerInput(SDL_Event _event)
+{
+	switch (_event.type)
+	{
+	case SDL_EVENT_KEY_DOWN:
+		if (m_playerCanMove)
+		{
+			if (_event.key.key == SDLK_LEFT)
+			{
+				m_posX -= m_playerStep;
+				m_rect = { m_posX, m_posY, 120,120 };
+				m_playerCanMove = false;
+
+			}
+			if (_event.key.key == SDLK_RIGHT)
+			{
+				m_posX += m_playerStep;
+				m_rect = { m_posX, m_posY, 120,120 };
+				m_playerCanMove = false;
+			}
+			if (_event.key.key == SDLK_UP)
+			{
+				m_posY -= m_playerStep;
+				m_rect = { m_posX, m_posY, 120,120 };
+				m_playerCanMove = false;
+			}
+			if (_event.key.key == SDLK_DOWN)
+			{
+				m_posY += m_playerStep;
+				m_rect = { m_posX, m_posY, 120,120 };
+				m_playerCanMove = false;
+			}
+		}
+		break;
+	case SDL_EVENT_KEY_UP:
+		if (_event.key.key == SDLK_LEFT)
+		{
+			m_playerCanMove = true;
+		}
+		if (_event.key.key == SDLK_RIGHT)
+		{
+			m_playerCanMove = true;
+		}
+		if (_event.key.key == SDLK_UP)
+		{
+			m_playerCanMove = true;
+		}
+		if (_event.key.key == SDLK_DOWN)
+		{
+			m_playerCanMove = true;
+		}
+		break;
+	}
+}
+
+
